1cd0a4f32dfbca401d13d9238715c16083c61201
Sandesh Pathak <sandeshpathak0112@gmail.com>
2017-08-09 13:27:34 -0400

    video list click

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-import  VideoList from './components/video_list'
+import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
 
 const API_KEY =  'AIzaSyBbvucyrLQWa8gRQBSmmx-CA_QcwPUgBlw';
 
@@ -11,16 +12,28 @@ class App extends Component{
 	constructor(props) {
 		super(props);
 
-		this.state = { videos: [] };
+		this.state = { 
+			videos: [],
+			selectedVideo: null
+		 };
 
-		YTSearch({key:API_KEY, term: 'calisthenics'}, (videos)=> this.setState({videos}));
+		YTSearch({key:API_KEY, term: 'edm'}, (videos)=> {
+			this.setState({
+				videos:videos,
+				selectedVideo:videos[0]
+			})
+		});
 
 	}
-	render(){ return (
-		<div>
-			<SearchBar />
-			<VideoList videos = {this.state.videos}/>
-		</div>
+	render(){ 
+		return (
+			<div>
+				<SearchBar />
+				<VideoDetail video = {this.state.selectedVideo}/>
+				<VideoList 
+					onVideoSelect = {selectedVideo => this.setState(selectedVideo)}
+					videos = {this.state.videos}/>
+			</div>
 		)
 	}
 } 
